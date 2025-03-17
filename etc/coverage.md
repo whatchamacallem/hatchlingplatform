6cee74e1a7936f2a57059d235cb306d2a136c971
Adrian Johnston <adrian3@gmail.com>
2025-03-16 18:27:49 -0700

    update build

--- etc/coverage.md
+++
@@ -1,9 +0,0 @@
-
-The HatchlingPlatform is built and run on travis-ci.org and the results sent to codecov.io using etc/coverage.sh.  The badge
-below links to codecov.io.
-
-[![codecov](https://codecov.io/gh/adrian3git/HatchlingPlatform/branch/master/graph/badge.svg)](https://codecov.io/gh/adrian3git/HatchlingPlatform)
-
-The C code for printf has full coverage on codecov.io as well.
-
-[![codecov](https://codecov.io/gh/mpaland/printf/branch/master/graph/badge.svg)](https://codecov.io/gh/mpaland/printf)
---
+++ etc/debugbuild.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright 2025 Adrian Johnston
+
+set -o errexit
+
+# c++ warning flags.  preceded by -pedantic-errors.
+WARNINGS="-Wall -Wextra -Werror -Wcast-qual -Wdisabled-optimization -Wshadow \
+	-Wwrite-strings -Wundef -Wendif-labels -Wstrict-overflow=1 -Wunused-parameter"
+
+I=0
+
+set -o xtrace
+
+gcc -Iinclude -O$I -g -Wall -Wextra -Werror -pedantic-errors -DHX_RELEASE=$I "$@" \
+	-std=c99 -c src/*.c
+gcc -Iinclude -O$I -g -pedantic-errors $WARNINGS -DHX_RELEASE=$I "$@" -pthread \
+	-std=c++14 -fno-exceptions -fno-rtti */*.cpp *.o -lpthread -lstdc++ -o hxtest
+
