f93a45e350ee4baa989a9adb90eb0888421816e2
Adrian Johnston <adrian3@gmail.com>
2025-06-14 16:35:57 -0700

    launch chrome with coverage results

--- testcoverage.sh
+++ testcoverage.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-set -o xtrace
 set -o errexit
+set -o xtrace
 
-gcc -Iinclude --coverage -O0 -Wall -DHX_RELEASE=0 -std=c99 -c src/*.c test/*.c
+gcc -Iinclude --coverage -O0 -DHX_RELEASE=0 -std=c99 -c src/*.c test/*.c
 
-g++ -Iinclude --coverage -O0 -Wall -DHX_RELEASE=0 -DHX_USE_CPP_THREADS=1 \
-	-DHX_MEM_DIAGNOSTIC_LEVEL=0 -DHX_TEST_ERROR_HANDLING=1 \
+g++ -Iinclude --coverage -O0 -DHX_RELEASE=0 -DHX_TEST_ERROR_HANDLING=1 \
 	-fno-exceptions -pthread -lpthread -std=c++17 -lstdc++ \
 	*/*.cpp *.o -o hxtest
 
@@ -13,9 +12,11 @@ echo runtests | ./hxtest printhashes help execstdin
 
 gcovr --html-details coverage.html
 
-# turn off tracing silently and make sure the command returns 0.
+# turn off tracing silently.
 { set +x; } 2> /dev/null
-echo 🐉🐉🐉
 
-# clean up with:
-# git clean -f -d
+# Launch Chrome if it is installed.
+[ -x /usr/bin/google-chrome ] && /usr/bin/google-chrome coverage.html
+
+# Make sure the script returns 0.
+echo 🐉🐉🐉
