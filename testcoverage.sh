d13b1a6fbf1a52c1cd99fe38c1f36f302ad13d35
Adrian Johnston <adrian3@gmail.com>
2025-06-19 15:24:15 -0700

    fix gcovr for bin dir

--- testcoverage.sh
+++ testcoverage.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export POSIXLY_CORRECT=1
+# POSIXLY_CORRECT=1 was breaking gcovr.
+# Using a subdirectory was breaking gcovr. See "--root .. ."
 
 set -o errexit
 
@@ -13,19 +14,19 @@ cd ./bin
 
 set -x
 
-gcc -I$HX_DIR/include --coverage -O0 -DHX_RELEASE=0 -std=c99 -ggdb3 \
+gcc -I$HX_DIR/include --coverage -Og -DHX_RELEASE=0 -std=c99 \
 	-c $HX_DIR/src/*.c $HX_DIR/test/*.c
 
-g++ -I$HX_DIR/include --coverage -O0 -DHX_RELEASE=0 -DHX_TEST_ERROR_HANDLING=1 \
-	-fno-exceptions -pthread -lpthread -std=c++17 -lstdc++ -ggdb3 \
+g++ -I$HX_DIR/include --coverage -Og -DHX_RELEASE=0 -DHX_TEST_ERROR_HANDLING=1 \
+	-fno-exceptions -pthread -lpthread -std=c++11 -lstdc++ \
 	$HX_DIR/*/*.cpp *.o -o hxtest
 
-echo runtests | ./hxtest printhashes help "checkhash 0" execstdin
+echo runtests | ./hxtest help printhashes "checkhash 0" execstdin
 
-gcovr --html-details coverage.html
+gcovr --html-details coverage.html --root .. .
 
 # turn off tracing silently.
-{ set +x; } &>/dev/null
+{ set +x; } 2> /dev/null
 
 # Launch Chrome if it is installed.
 [ "$1" != "--headless" ] && [ -x /usr/bin/google-chrome ] \
