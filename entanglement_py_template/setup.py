954ad24d0d6806538bb82d2d36226f9d0792f4ea
Adrian Johnston <adrian3@gmail.com>
2025-07-10 14:45:21 -0700

    wip

--- entanglement_py_template/setup.py
+++ entanglement_py_template/setup.py
@@ -9,11 +9,15 @@ import subprocess
 from typing import List
 
 # C++ build command.  E.g. cmake, make, ./build.sh.
-SETUP_CPP_COMMAND = ["./setup_cpp.sh"]
+SETUP_CPP_COMMAND = ['./setup_cpp.sh']
 
-LIBRARY="libentanglement_py_template.so.1"
-HEADER_FILES=["entanglement_cpp_test.hpp"]
-OUTPUT_FILE="entanglement_py_template.py"
+LIBRARY='libentanglement_py_template.so.1'
+HEADER_FILES=['entanglement_cpp_test.hpp']
+OUTPUT_FILE='entanglement_py_template.py'
+
+ENTANGLEMENT_COMMAND=['python3', 'entanglement.py', '-std=c++17', '-DHX_BINDINGS_PASS=1',
+    '-I../include', '-DHX_RELEASE=0', '-fdiagnostics-absolute-paths',
+    '-Wfatal-errors', LIBRARY] + HEADER_FILES + [OUTPUT_FILE]
 
 VERBOSE = 1
 
@@ -23,6 +27,7 @@ def _verbose(x: str) -> None:
     if VERBOSE:
         print(x)
 
+# Takes an argv and throws an exception if it doesn't return 0.
 def _run_argv(argv: List[str]) -> None:
     try:
         _verbose(' '.join(argv))
@@ -39,6 +44,7 @@ def _run_argv(argv: List[str]) -> None:
     except subprocess.CalledProcessError as e:
         if e.stdout:
             print(e.stdout, file=sys.stderr)
+        print(e, file=sys.stderr)
         sys.exit(e.returncode if e.returncode else _exit_error)
 
     except Exception as e:
@@ -47,13 +53,16 @@ def _run_argv(argv: List[str]) -> None:
 
 # Run Commands #
 
-_run_argv(SETUP_CPP_COMMAND)
+#_run_argv(SETUP_CPP_COMMAND)
 
-#_run_argv(['python3', 'entanglement.py', '-std=c++17', '-DHX_BINDINGS_PASS=1',
-#        LIBRARY] + HEADER_FILES + [OUTPUT_FILE])
+_run_argv(ENTANGLEMENT_COMMAND)
+
+_run_argv(['python3', 'entanglement_py_template.py'])
 
 # This is legacy. The manifest is now supposed to be in the .toml file.
-setuptools.setup(
-    install_requires=[],
-    python_requires='>=3.7',
-)
+#setuptools.setup(
+#    install_requires=[],
+#    python_requires='>=3.7',
+#)
+
+print("🐉🐉🐉")
