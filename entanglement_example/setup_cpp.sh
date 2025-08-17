aa658ba2dbdcb21564614ce19947082a7a76db37
Adrian Johnston <adrian3@gmail.com>
2025-08-17 16:52:47 -0700

    package installs

--- entanglement_example/setup_cpp.sh
+++ entanglement_example/setup_cpp.sh
@@ -25,9 +25,9 @@ set -o xtrace
 #clang++ $HX_CFLAGS -std=c++17 -fvisibility=hidden \
 #	-c $HX_DIR/src/*.cpp $HX_DIR/test/*.cpp
 
-# entanglement_example/*.cpp bin/*.o -> bin/hxtest
+# The .toml requires the .so to be in the src directory.
 clang++ $HX_CFLAGS $HX_LDFLAGS -std=c++17 -lstdc++ -lpthread \
-	$HX_DIR/entanglement_example/src/*.cpp -o libentanglement_py_template.so.1
+	$HX_DIR/entanglement_example/src/*.cpp -o src/libentanglement_py_template.so.1
 
 { set +o xtrace; } 2> /dev/null
 rm -f *.o
