eea8609058a06e8d45d8eef16514c82b17d731d1
Adrian Johnston <adrian3@gmail.com>
2025-10-05 00:35:47 -0700

    +hxtest_macros test relational test macros

--- test/hxtest_test.cpp
+++ test/hxtest_test.cpp
@@ -5,8 +5,52 @@
 #include <hx/hxtest.hpp>
 #include <hx/hxarray.hpp>
 
+#include "hxctest.h"
+
+#include <ctype.h>
+
 HX_REGISTER_FILENAME_HASH
 
+namespace {
+
+template<typename T>
+void hxtest_relational_(T a, T b) {
+	ASSERT_EQ(a, a);
+	ASSERT_GE(a, a);
+	ASSERT_GE(b, a);
+	ASSERT_GT(b, a);
+	ASSERT_LE(a, a);
+	ASSERT_LE(a, b);
+	ASSERT_LT(a, b);
+	ASSERT_NE(a, b);
+	EXPECT_EQ(a, a);
+	EXPECT_GE(a, a);
+	EXPECT_GE(b, a);
+	EXPECT_GT(b, a);
+	EXPECT_LE(a, a);
+	EXPECT_LE(a, b);
+	EXPECT_LT(a, b);
+	EXPECT_NE(a, b);
+}
+
+} // namespace {
+
+TEST(hxtest_macros, relational) {
+	hxtest_relational_<int>(-1, 0);
+	hxtest_relational_<long>(-5, -4);
+	hxtest_relational_<long long>(-9, -8);
+	hxtest_relational_<unsigned int>(1u, 2u);
+	hxtest_relational_<unsigned long>(5ul, 6ul);
+	hxtest_relational_<unsigned long long>(11ull, 12ull);
+	hxtest_relational_<int8_t>(static_cast<int8_t>(-12), static_cast<int8_t>(-11));
+	hxtest_relational_<uint8_t>(static_cast<uint8_t>(3), static_cast<uint8_t>(4));
+	hxtest_relational_<int16_t>(static_cast<int16_t>(-301), static_cast<int16_t>(-300));
+	hxtest_relational_<uint16_t>(static_cast<uint16_t>(7), static_cast<uint16_t>(8));
+	hxtest_relational_<int32_t>(static_cast<int32_t>(-70001), static_cast<int32_t>(-70000));
+	hxtest_relational_<uint32_t>(static_cast<uint32_t>(100), static_cast<uint32_t>(101));
+	hxtest_relational_<float>(-0.00002f, -0.00001f);
+	hxtest_relational_<double>(0.0, 1.0);
+}
 
 // Run all the C tests.
 TEST(hxctest, all_tests) {
