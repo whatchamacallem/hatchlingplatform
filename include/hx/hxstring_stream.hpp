a3acfd9220d1fd78ef3d393714b5eea3a3035942
Adrian Johnston <adrian3@gmail.com>
2025-10-07 12:53:11 -0700

    misc

--- include/hx/hxstring_stream.hpp
+++ include/hx/hxstring_stream.hpp
@@ -18,16 +18,16 @@ public:
 
 	void operator=(hxstring_stream&& other_);
 
-	operator bool(void) const { return m_good_; }
+	operator bool(void) const { return !m_failed_; }
 
-	bool good(void) const { return m_good_; }
+	bool fail_(void) const { return m_failed_; }
 	bool eof(void) const { return m_eof_; }
 	void clear(void);
 
 	size_t get_pos(void) const { return m_position_; }
 	bool set_pos(size_t position_);
-	size_t read(void* bytes_, size_t count_) hxattr_nonnull(2) hxattr_hot;
-	size_t write(const void* bytes_, size_t count_) hxattr_nonnull(2) hxattr_hot;
+	size_t read(char* bytes_, size_t count_) hxattr_nonnull(2) hxattr_hot;
+	size_t write(const char* bytes_, size_t count_) hxattr_nonnull(2) hxattr_hot;
 
 	template<size_t buffer_size_>
 	bool getline(char(&buffer_)[buffer_size_]) {
@@ -35,18 +35,6 @@ public:
 	}
 
 	bool getline(char* buffer_, int buffer_size_) hxattr_nonnull(2) hxattr_hot;
-
-	template<typename T_>
-	hxstring_stream& operator>=(T_& t_) {
-		this->read(&t_, sizeof t_);
-		return *this;
-	}
-
-	template<typename T_>
-	hxstring_stream& operator<=(const T_& t_) {
-		this->write(&t_, sizeof t_);
-		return *this;
-	}
 
 	template<size_t string_length_>
 	hxstring_stream& operator<<(const char(&str_)[string_length_]) {
@@ -67,6 +55,6 @@ private:
 	hxarray<char, hxallocator_dynamic_capacity> m_buffer_; // Contains capacity.
 	size_t m_size_;
 	size_t m_position_;
-	bool m_good_; // Has an error been encountered.
+	bool m_failed_; // Has any error been encountered.
 	bool m_eof_; //
 };
