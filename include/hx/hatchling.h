05904e2b6243e61da6289df3ed72997b898a8c7e
A Johnston <ajohnston54637@gmail.com>
2025-11-04 18:43:48 -0800

    v3.31.0-dev

--- include/hx/hatchling.h
+++ include/hx/hatchling.h
@@ -32,11 +32,11 @@
 
 /// `int HATCHLING_VER` - One digit major, and two digit minor and patch
 /// versions. Odd numbered minor versions are development branches.
-#define HATCHLING_VER 33000
+#define HATCHLING_VER 33100
 
 /// `HATCHLING_TAG` - Major, minor and patch version tag name. Odd numbered
 /// minor versions are development branches and their tags end in `-dev`.
-#define HATCHLING_TAG "v3.30.0"
+#define HATCHLING_TAG "v3.31.0-dev"
 
 #include "hxsettings.h"
 #include "hxmemory_manager.h"
